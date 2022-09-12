fa0a685cd3dff3a8ca15171850fc65627012ab7b
Srimanta Pal <srimantapal205@hotmail.com>
2022-09-12 23:44:08 +0530

    Todo App setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 class="text-center">Vue-3 ToDo App</h1>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col">
+        <form class="d-flex" @submit="addNewTodo">
+          <div class="col-8">
+            <div class="mb-3">
+              <label for="" class="form-label">New Todo</label>
+              <input
+                type="text"
+                name="todo"
+                id="newTodo"
+                class="form-control"
+                placeholder="Add new Todo"
+                aria-describedby="helpId"
+              />
+            </div>
+          </div>
+          <div class="col-4 text-center">
+            <button type="button" class="btn btn-primary mt-4">Add New Todo </button>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { defineComponent } from "vue";
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+export default defineComponent({});
 </script>
+
+<style lang="scss" scoped></style>
